014ca4832fa2d75189a72d8be7d3473bb8207d15
Paul Baker <paul.nelson.baker@gmail.com>
2018-07-01 03:27:25 -0600

    Adding initial test to project

--- src/test/java/io/paulbaker/gradle/shadow/transform/PluginsCacheFileTransformerTests.java
+++ src/test/java/io/paulbaker/gradle/shadow/transform/PluginsCacheFileTransformerTests.java
@@ -1,10 +1,46 @@
 package io.paulbaker.gradle.shadow.transform;
 
+import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator;
+import com.github.jengelman.gradle.plugins.shadow.relocation.SimpleRelocator;
+import com.github.jengelman.gradle.plugins.shadow.transformers.TransformerContext;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.logging.log4j.core.config.plugins.processor.PluginProcessor.PLUGIN_CACHE_FILE;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * @author Paul Nelson Baker
- * WGU Student Info: pbake22 - #000412290
  * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
  * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
  */
 public class PluginsCacheFileTransformerTests {
+
+    private final URL PLUGIN_URL = getResourceUrl(PLUGIN_CACHE_FILE);
+
+    private static URL getResourceUrl(String resource) {
+        return PluginsCacheFileTransformerTests.class.getClass().getClassLoader().getResource(resource);
+    }
+
+    private static InputStream getResourceStream(String resource) {
+        return PluginsCacheFileTransformerTests.class.getClass().getClassLoader().getResourceAsStream(resource);
+    }
+
+    @Test
+    public void test() {
+        PluginsCacheFileTransformer transformer = new PluginsCacheFileTransformer();
+        final InputStream inputStream = getResourceStream(PLUGIN_CACHE_FILE);
+        transformer.transform(new TransformerContext(PLUGIN_CACHE_FILE, inputStream, null));
+        assertFalse(transformer.hasTransformedResource());
+
+        List<Relocator> relocators = new ArrayList<>();
+        relocators.add(new SimpleRelocator(null, null, null, null));
+        new TransformerContext(PLUGIN_CACHE_FILE, inputStream, relocators);
+        assertTrue(transformer.hasTransformedResource());
+    }
 }
