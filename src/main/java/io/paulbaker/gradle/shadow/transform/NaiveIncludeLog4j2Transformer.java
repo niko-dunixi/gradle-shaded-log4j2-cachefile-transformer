e14920d53173665c2e3123b4b992fd88f5d6d5b1
Paul Baker <paul.nelson.baker@gmail.com>
2018-07-01 08:20:22 -0600

    not a noop plugin anymore.

--- src/main/java/io/paulbaker/gradle/shadow/transform/NaiveIncludeLog4j2Transformer.java
+++ src/main/java/io/paulbaker/gradle/shadow/transform/NaiveIncludeLog4j2Transformer.java
@@ -2,16 +2,22 @@ package io.paulbaker.gradle.shadow.transform;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+
+import java.util.Set;
 
 /**
  * @author Paul Nelson Baker
- * WGU Student Info: pbake22 - #000412290
  * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
  * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
  */
-public class NoopPlugin implements Plugin<Project> {
+public class NaiveIncludeLog4j2Transformer implements Plugin<Project> {
 
     @Override
     public void apply(Project target) {
+        Set<Task> shadowJarTasks = target.getTasksByName("shadowJar", false);
+        for (Task currentTask : shadowJarTasks) {
+            currentTask.setProperty("transform", PluginsCacheFileTransformer.class);
+        }
     }
 }
