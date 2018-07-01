2aa2b6e160a94dd24f0b01070eea189b5012c613
Paul Baker <paul.nelson.baker@gmail.com>
2018-07-01 04:50:26 -0600

    Adding tests to validate transformations

--- src/test/java/io/paulbaker/gradle/shadow/transform/PluginsCacheFileTransformerTests.java
+++ src/test/java/io/paulbaker/gradle/shadow/transform/PluginsCacheFileTransformerTests.java
@@ -5,12 +5,19 @@ import com.github.jengelman.gradle.plugins.shadow.relocation.SimpleRelocator;
 import com.github.jengelman.gradle.plugins.shadow.transformers.TransformerContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import shadow.org.apache.tools.zip.ZipOutputStream;
 
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
+import static java.util.Collections.singletonList;
 import static org.apache.logging.log4j.core.config.plugins.processor.PluginProcessor.PLUGIN_CACHE_FILE;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -50,5 +57,40 @@ public class PluginsCacheFileTransformerTests {
         relocators.add(new SimpleRelocator(null, null, null, null));
         transformer.transform(new TransformerContext(PLUGIN_CACHE_FILE, getResourceStream(PLUGIN_CACHE_FILE), relocators));
         assertTrue(transformer.hasTransformedResource());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "org.apache.logging, new.location.org.apache.logging, new.location.org.apache.logging",
+            "org.apache.logging, new.location.org.apache.logging, org.apache.logging",
+    })
+    public void testRelocate(String source, String pattern, String target) throws IOException {
+        List<Relocator> relocators = singletonList((Relocator) new SimpleRelocator(source, pattern, null, null));
+        transformer.transform(new TransformerContext(PLUGIN_CACHE_FILE, getResourceStream(PLUGIN_CACHE_FILE), relocators));
+        assertTrue(transformer.hasTransformedResource(), "Transformer didn't transform resources");
+        // Write out to a fake jar file
+        File testableZipFile = File.createTempFile("testable-zip-file-", ".jar");
+        try (FileOutputStream fileOutputStream = new FileOutputStream(testableZipFile)) {
+            try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)) {
+                try (ZipOutputStream zipOutputStream = new ZipOutputStream(bufferedOutputStream)) {
+                    transformer.modifyOutputStream(zipOutputStream);
+                }
+            }
+        }
+        // Pull the data back out and make sure it was transformed
+        ZipFile zipFile = new ZipFile(testableZipFile);
+        ZipEntry zipFileEntry = zipFile.getEntry(PLUGIN_CACHE_FILE);
+        InputStream inputStream = zipFile.getInputStream(zipFileEntry);
+        try (Scanner scanner = new Scanner(inputStream)) {
+            boolean hasAtLeastOneTransform = false;
+            while (scanner.hasNextLine()) {
+                String nextLine = scanner.nextLine();
+                if (nextLine.contains(source)) {
+                    hasAtLeastOneTransform = true;
+                    assertTrue(nextLine.contains(target), "Target wasn't included in transform");
+                }
+            }
+            assertTrue(hasAtLeastOneTransform, "There were no transformations inside the file");
+        }
     }
 }
