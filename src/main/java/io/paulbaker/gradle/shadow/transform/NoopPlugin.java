0630af85bf1a5960a2851ff3fec09fcd62988997
Paul Baker <paul.nelson.baker@gmail.com>
2018-07-01 07:33:35 -0600

    Fixing stuff

--- src/main/java/io/paulbaker/gradle/shadow/transform/NoopPlugin.java
+++ src/main/java/io/paulbaker/gradle/shadow/transform/NoopPlugin.java
@@ -1,10 +1,17 @@
 package io.paulbaker.gradle.shadow.transform;
 
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
 /**
  * @author Paul Nelson Baker
  * WGU Student Info: pbake22 - #000412290
  * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
  * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
  */
-public class NoopPlugin {
+public class NoopPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+    }
 }
