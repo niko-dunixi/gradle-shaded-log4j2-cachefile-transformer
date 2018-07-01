378181cb7dcac212fb20c1204ec48fe4ae7b2e64
Paul Baker <paul.nelson.baker@gmail.com>
2018-07-01 03:25:48 -0600

    JUnit 5 tests

---
+++ src/test/java/io/paulbaker/gradle/shadow/transform/PluginsCacheFileTransformerTests.java
@@ -0,0 +1,10 @@
+package io.paulbaker.gradle.shadow.transform;
+
+/**
+ * @author Paul Nelson Baker
+ * WGU Student Info: pbake22 - #000412290
+ * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
+ * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
+ */
+public class PluginsCacheFileTransformerTests {
+}
