a5c99b16a06936ea4e926b6e243a75f7d38a9a92
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-08-15 20:30:03 -0600

    Removing plugin so we can just get the transformer on the classpath

---
+++ src/main/java/io/paulbaker/gradle/shadow/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author Paul Nelson Baker
+ * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
+ * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
+ * @since 2018-08
+ * WGU Student Info: pbake22 - #000412290
+ */
+package io.paulbaker.gradle.shadow;
